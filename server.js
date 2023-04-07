829a0e5093b73f503eeefc309587794822b66c61
Pat Nordstrom <pnordstr@akamai.com>
2023-04-07 11:13:18 -0500

    added data to response

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const os = require('os');
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  var currentTime = new Date();
+  res.send('Response from ' + os.hostname() + ' at ' + currentTime + '\n');
 });
 
 app.listen(PORT, HOST, () => {
